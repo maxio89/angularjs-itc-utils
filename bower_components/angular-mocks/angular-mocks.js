59b5acb3478103462aa21ebdf174eac92d7e230b
Piotr Kozlowski <piotr.kozlowski@itcrowd.pl>
2014-01-06 23:58:24 +0100

    Demo page and other improvements

--- bower_components/angular-mocks/angular-mocks.js
+++ bower_components/angular-mocks/angular-mocks.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.7-build.2036+sha.e415e91
+ * @license AngularJS v1.2.8-build.2073+sha.a7aa4cc
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -1577,6 +1577,10 @@ function MockHttpExpectation(method, url, data, headers) {
   this.toString = function() {
     return method + ' ' + url;
   };
+}
+
+function createMockXhr() {
+  return new MockXhr();
 }
 
 function MockXhr() {
