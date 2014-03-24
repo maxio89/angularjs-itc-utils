c32e8fde5f9e8646b1e081c8a58f55a3ec90a916
Piotr Kozlowski <piotr.kozlowski@itcrowd.pl>
2014-03-24 10:37:28 +0100

    Prepare for release

--- modules/constants/validationMessages.js
+++ modules/constants/validationMessages.js
@@ -12,8 +12,26 @@
                     minlength: 'Enter more characters!',
                     maxlength: 'You have entered to many characters!',
                     equals: 'Password doesn\'t match!',
+                    lessthan: 'Value must be less than other!',
+                    float: 'Value must be a decimal number',
                     unique: 'Your email should be unique!',
-                    pattern: 'Characters don\'t match to the pattern!'
+                    pattern: 'Characters don\'t match to the pattern!',
+                    'unique-user-email': 'This email is already registered!',
+                    'unique-product-permalink': 'This permalink already exists!',
+                    'unique-shop-permalink': 'This permalink already exists!'
+                },
+                pl: {
+                    required: 'Wartość wymagana!',
+                    email: 'Wprowadź poprawny adres email!',
+                    minlength: 'Za mało znaków!',
+                    maxlength: 'Za dużo znaków!',
+                    equals: 'Hasło nie pasuje!',
+                    float: 'Wartość musi być liczbą',
+                    unique: 'Email musi być unikalny. To będzie twój login!',
+                    pattern: 'Podane znaki nie są dopuszczalne!',
+                    'unique-user-email': 'Ten adres jest już zarejestrowany',
+                    'unique-product-permalink': 'Taki permalink już istnieje!',
+                    'unique-shop-permalink': 'Taki permalink już istnieje!'
                 }
             }
         }
